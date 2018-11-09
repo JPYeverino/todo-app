f448241e87711c952430583a262bf57e2ff466d2
JPablo Yeverino <jp.yeverinom@gmail.com>
2018-11-09 13:03:41 -0800

    Test POST /todos route

---
+++ src/server/__tests__/server.test.ts
@@ -0,0 +1,32 @@
+import * as request from 'supertest';
+
+import app from '../server';
+import {Todo} from '../models/todo';
+
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe("POST /todos", () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+    request(app)
+    .post('/todos')
+    .send({text})
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.text).toBe(text);
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch(e => done(e));
+    })
+  });
+});
+
