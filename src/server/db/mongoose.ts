738495c7649792562741fc8fd687b22fff277d84
JPablo Yeverino <jp.yeverinom@gmail.com>
2018-11-10 14:58:49 -0800

    Correct mongodb connection at mongoose.ts

--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -1,3 +1,3 @@
 import * as mongoose from 'mongoose';
 
-let s = mongoose.connect('process.ENV.MONGODB_URI || mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
