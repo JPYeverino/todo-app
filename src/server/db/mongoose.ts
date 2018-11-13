7dd2b3530dfd335ebadb79442072663fbfd3f91d
JPablo Yeverino <jp.yeverinom@gmail.com>
2018-11-13 13:48:35 -0800

    Setup typescript automation with nodemon and separate test and development envs

--- src/server/db/mongoose.ts
+++ src/server/db/mongoose.ts
@@ -1,3 +1,3 @@
 import * as mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+  // mongoose.connect(process.env.MONGODB_URI );
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,25 @@
 import * as _ from 'lodash';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
-import { ObjectID, ObjectId } from 'mongodb';
+import * as mongoose from 'mongoose';
 
-import './db/mongoose';
+import { ObjectID, ObjectId } from 'mongodb';
 import { User } from './models/user';
 import { Todo } from './models/todo';
-
-const port = process.env.PORT || 3000;
 const app: express.Application = express();
+
+let env = process.env.NODE_ENV || 'development';
+
+if( env === 'development') {
+  process.env.PORT = '3000';
+  process.env.MONGODB_URI ='mongodb://localhost:27017/TodoApp';
+} else if( env === 'test') {
+  process.env.PORT = '3000';
+  process.env.MONGODB_URI ='mongodb://localhost:27017/TodoAppTest';
+}
+
+console.log('env ***', env);
+
 
 app.use(bodyParser.json());
 
@@ -87,8 +98,10 @@ app.patch('/todos/:id', (req, res) => {
     .catch(e => res.status(404).send);
 });
 
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Started on port ${process.env.PORT}`);
 });
+
+mongoose.connect(process.env.MONGODB_URI || "");
 
 export default app;
